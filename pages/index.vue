a3216af51e372b63523aa9af0dac0b1f14327795
1Mill <erik.ekberg@outlook.com>
2018-09-08 15:31:00 -0700

    Outline instructions

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,11 @@
 <template>
-	<div>
-		HELLO
-	</div>
+	<ol>
+		<li>Check the boxes for each race you want to play with</li>
+		<li>Determine the number of players and/or Enter names</li>
+		<li>Number of Choices per player</li>
+		<li>Optional Speaker options</li>
+		<li>Deal out races</li>
+	</ol>
 </template>
 
 <script>
