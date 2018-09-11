874ae5a3cb122c8e21f8c3a7a64b7127d4a44c78
1Mill <erik.ekberg@outlook.com>
2018-09-10 17:56:15 -0700

    Demonstrate use of races / races_active

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,35 @@
 <template>
-	<ol>
-		<li>Check the boxes for each race you want to play with</li>
-		<li>Determine the number of players and/or Enter names</li>
-		<li>Number of Choices per player</li>
-		<li>Optional Speaker options</li>
-		<li>Deal out races</li>
-	</ol>
+	<div>
+		<ol>
+			<li>Check the boxes for each race you want to play with</li>
+			<li>Determine the number of players and/or Enter names</li>
+			<li>Number of Choices per player</li>
+			<li>Optional Speaker options</li>
+			<li>Deal out races</li>
+		</ol>
+		<div
+		v-for='race in races' :key='race.name'
+		>
+			{{ race.name }} {{ race.active }}
+		</div>
+
+		<div>
+			{{ active_races }}
+		</div>
+	</div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
+export default {
+	computed: {
+		...mapGetters([
+			'races',
+			'active_races'
+		])
+	}
+}
 </script>
 
 <style>
