bd493129b2317058d3911c831da69314e405c258
1Mill <erik.ekberg@outlook.com>
2018-09-12 13:41:50 -0700

    Add player count

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,12 +1,23 @@
 <template>
 	<div>
 		<ol>
-			<li>Check the boxes for each race you want to play with</li>
 			<li>Determine the number of players and/or Enter names</li>
+			<li>Check the boxes for each race you want to play with</li>
 			<li>Number of Choices per player</li>
 			<li>Optional Speaker options</li>
 			<li>Deal out races</li>
 		</ol>
+
+		<label>Player Count: </label>
+		<select v-model='player_count'>
+			<option value="1">1</option>
+			<option value="2">2</option>
+			<option value="3">3</option>
+			<option value="4">4</option>
+			<option value="5">5</option>
+			<option value="6">6</option>
+		</select>
+
 		<div
 		v-for='race in races' :key='race.name'
 		>
@@ -26,6 +37,12 @@
 import { mapActions, mapGetters } from 'vuex'
 
 export default {
+	data: function () {
+		return {
+			player_count: 6
+		}
+	},
+
 	computed: {
 		...mapGetters([
 			'races'
