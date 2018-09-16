3fea9fe1c99b619178b816b1168333a8fa181162
1Mill <erik.ekberg@outlook.com>
2018-09-15 18:32:49 -0700

    Style buttons
    Use this link as a guide: https://styleguide.studyportals.com/student/components/buttons

---
+++ client/components/Button.vue
@@ -0,0 +1,55 @@
+<template>
+	<a
+	:class='getClassStyle'
+	>
+		<slot />
+	</a>
+</template>
+
+<script>
+export default {
+	props: {
+		styling: {
+			required: true,
+			type: String
+		}
+	},
+
+	computed: {
+		getClassStyle: function () {
+			let class_style = 'dib br1'
+
+			switch(this.styling) {
+				// Champion
+				case 'c':
+					class_style += '[ pv3 ph4 white bg-brand-first hover-bg-brand-first-dark _Button ]'
+					break
+
+				// Driver
+				case 'd':
+					class_style += '[ pv2 ph3 brand-first ba bw1 hover-brand-first-dark  ]'
+					break
+
+				// Driver White
+				case 'dw':
+					class_style += '[ pv2 ph3 white ba bw1 hover-moon-gray ]'
+					break
+
+				// Helper
+				case 'h':
+					class_style += '[ pv2 ph3 brand-second hover-brand-second-dark underline-hover ]'
+					break
+
+				// Helper white
+				case 'hw':
+					class_style += '[ pv2 ph3 moon-gray hover-silver underline-hover ]'
+			}
+
+			return class_style
+		}
+	}
+}
+</script>
+
+<style scoped>
+</style>
--- client/pages/testing.vue
+++ client/pages/testing.vue
@@ -1,11 +1,26 @@
 <template>
-	<div>
-		TESITNG PAGE
+	<div class='[ mt5 tc ]'>
+		<Button styling='c'>Button text</Button>
+		<br /><br />
+		<Button styling='d'>Button text</Button>
+		<br /><br />
+		<div class='[ pv2 bg-gray ]'>
+			<Button styling='dw'>Button text</Button>
+		</div>
+		<br />
+		<Button styling='h'>Button text</Button>
+		<br /><br />
+		<Button styling='hw'>Button text</Button>
 	</div>
 </template>
 
 <script>
+import Button from '~/components/Button'
+
 export default {
+	components: {
+		Button
+	}
 }
 </script>
 
