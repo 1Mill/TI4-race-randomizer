06a96f2168729c97426a8632acd41f84eae78c89
1Mill <erik.ekberg@outlook.com>
2018-09-19 16:21:37 -0700

    Add minor support for not-small screens

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,11 +1,13 @@
 <template>
-	<div>
-		<nuxt
-		class='[ mb4 ]'
-		/>
-		<div class='[ pv2 bg-brand-second flex flex-row justify-around ]'>
-			<Button styling='dw' href='/'>Home / Restart</Button>
-			<Button styling='hw' href='https://github.com/1Mill/twilight-imperium-race-selector' target='_blank'>Source code</Button>
+	<div class='[][ flex-ns flex-row-ns justify-center-ns ]'>
+		<div class='[][ w-50-ns  ]'>
+			<nuxt
+			class='[ mb4 ]'
+			/>
+			<div class='[ pv2 bg-brand-second flex flex-row justify-around ]'>
+				<Button styling='dw' href='/'>Home / Restart</Button>
+				<Button styling='hw' href='https://github.com/1Mill/twilight-imperium-race-selector' target='_blank'>Source code</Button>
+			</div>
 		</div>
 	</div>
 </template>
