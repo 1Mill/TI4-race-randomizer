ea3cc4e387703b61d9de8ac6cd3ee26f6b90108a
1Mill <erik.ekberg@outlook.com>
2018-09-19 14:59:05 -0700

    Add footer to layout

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,10 +1,24 @@
 <template>
 	<div>
 		<nuxt
-		class='[ mb3 ]'
+		class='[ mb4 ]'
 		/>
+		<div class='[ pv2 bg-brand-second flex flex-row justify-around ]'>
+			<Button styling='dw' href='/'>Home / Restart</Button>
+			<Button styling='hw' href='https://github.com/1Mill/twilight-imperium-race-selector' target='_blank'>Source code</Button>
+		</div>
 	</div>
 </template>
+
+<script>
+import Button from '~/components/Button'
+
+export default {
+	components: {
+		Button
+	}
+}
+</script>
 
 <style>
 </style>
