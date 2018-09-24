6e3aa63650a8e49728b7c3b84b657d41327231fa
1Mill <erik.ekberg@outlook.com>
2018-09-23 20:38:07 -0700

    Add race pages / index

---
+++ client/pages/races/_url_name.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		INDIVIDUAL RACE
+
+		<br /><br />
+
+		{{ race }}
+	</div>
+</template>
+
+<script>
+export default {
+	data: function () {
+		return {
+			race: {}
+		}
+	},
+
+	mounted: function () {
+		const url_name = this.$route.params.url_name
+
+		this.race = this.$store.state.races.find(race => race.urlName === url_name)
+	}
+}
+</script>
+
+<style scoped>
+</style>
---
+++ client/pages/races/index.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<div
+		v-for='race in races' :key='race.name'
+		>
+			<Button styling='d'
+			:href='"/races/" + race.urlName'
+			>
+				{{ race.name }} :: {{ race.urlName }}
+			</Button>
+		</div>
+	</div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import Button from '~/components/Button'
+
+export default {
+	computed: {
+		...mapState ([
+			'races'
+		])
+	},
+
+	components: {
+		Button
+	}
+}
+</script>
