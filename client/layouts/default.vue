51e19a0d753c14fa749a0a046ef916c6cf32d0de
1Mill <erik.ekberg@outlook.com>
2020-11-14 14:56:03 -0800

    Update default.vue

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -2,7 +2,7 @@
 	<div class='[][ flex-ns flex-row-ns justify-center-ns ]'>
 		<div class='[][ w-50-ns  ]'>
 			<div class='[ pa2 flex flex-row justify-between ]'>
-				<Button styling='h' href='/'>Home</Button>
+				<Button styling='h' href='/'>Homepage</Button>
 				<Button styling='hw' href='https://github.com/1Mill/twilight-imperium-race-selector' target='_blank'>Source code</Button>
 			</div>
 
