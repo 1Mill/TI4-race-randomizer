61b92e211dd94b0878dcb8343e599e2d3c833b4c
1Mill <erik.ekberg@outlook.com>
2020-11-14 15:46:40 -0800

    Add expansion symbols

--- client/components/SymbolLegend.vue
+++ client/components/SymbolLegend.vue
@@ -1,3 +1,6 @@
 <template>
-	<p class='[ lh-copy f7 ]'><b>*</b> races are recommended for new players</p>
+	<div class='[ mt3 mb3 ]'>
+		<p class='[ ma0 lh-copy f7 ]'><b>*</b> race recommended for new players</p>
+		<p class='[ ma0 lh-copy f7 ]'><b>+</b> Prophecy of Kings expansion race</p>
+	</div>
 </template>
--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class='[][ flex-ns flex-row-ns justify-center-ns ]'>
-		<div class='[][ w-50-ns  ]'>
+		<div class='[][ w-50-ns ]'>
 			<div class='[ pa2 flex flex-row justify-between ]'>
 				<Button styling='h' href='/'>Homepage</Button>
 				<Button styling='hw' href='https://github.com/1Mill/twilight-imperium-race-selector' target='_blank'>Source code</Button>
