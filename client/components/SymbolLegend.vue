3076b3a799e81a4da46a75def8122b2e62a77708
1Mill <erik.ekberg@outlook.com>
2022-05-31 20:31:58 -0700

    Change * to superscript 1 + language

--- client/components/SymbolLegend.vue
+++ client/components/SymbolLegend.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class='[ mt3 mb3 ]'>
-		<p class='[ ma0 lh-copy f7 ]'><b>*</b> race recommended for new players</p>
-		<p class='[ ma0 lh-copy f7 ]'><b>+</b> Prophecy of Kings expansion race</p>
+		<p class='[ ma0 lh-copy f7 ]'><b>1</b> recommended for new players</p>
+		<p class='[ ma0 lh-copy f7 ]'><b>+</b> Prophecy of Kings</p>
 	</div>
 </template>
