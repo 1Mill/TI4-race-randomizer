48fcc86c93aeaf3b84ec92e2480f2591289c283c
1Mill <erik.ekberg@outlook.com>
2022-05-31 20:32:12 -0700

    Add Council Keleres to list

--- client/components/SymbolLegend.vue
+++ client/components/SymbolLegend.vue
@@ -2,5 +2,6 @@
 	<div class='[ mt3 mb3 ]'>
 		<p class='[ ma0 lh-copy f7 ]'><b>1</b> recommended for new players</p>
 		<p class='[ ma0 lh-copy f7 ]'><b>+</b> Prophecy of Kings</p>
+		<p class='[ ma0 lh-copy f7 ]'><b>^</b> Codex</p>
 	</div>
 </template>
