bd85b4a6c6d2f9acc5f2876973ddadb2ce975900
1Mill <erik.ekberg@outlook.com>
2023-03-09 15:57:55 -0800

    Shift text left

--- client/components/SymbolLegend.vue
+++ client/components/SymbolLegend.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class='[ mt3 mb3 ]'>
+	<div class='[ mt3 mb3 tl ]'>
 		<p class='[ ma0 lh-copy f7 ]'><b>1</b> recommended for new players</p>
 		<p class='[ ma0 lh-copy f7 ]'><b>+</b> Prophecy of Kings</p>
 		<p class='[ ma0 lh-copy f7 ]'><b>=</b> Codex</p>
