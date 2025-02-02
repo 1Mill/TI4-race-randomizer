b9aafe6aa4aab977bb149be36e3742bf69326117
GothicModder <78573262+GothicModder@users.noreply.github.com>
2025-02-02 03:56:40 +0100

    Updated Legend for Discordant Stars
    Added 3 for 10 new factions from Discordant Stars

--- client/components/SymbolLegend.vue
+++ client/components/SymbolLegend.vue
@@ -3,5 +3,6 @@
 		<p class='[ ma0 lh-copy ]'><b>^</b> Recommended for new players</p>
 		<p class='[ ma0 lh-copy ]'><b>1</b> Prophecy of Kings</p>
 		<p class='[ ma0 lh-copy ]'><b>2</b> Codex</p>
+		<p class='[ ma0 lh-copy ]'><b>3</b> New 10 Discordant Stars factions</p>
 	</div>
 </template>
