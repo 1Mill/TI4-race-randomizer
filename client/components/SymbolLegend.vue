9b561e702d55ce3e92ae752a8f3e9f6dab9e8690
SkullBunny <ircarrigan@gmail.com>
2026-01-13 21:30:38 -0800

    Added New Thunder's Edge Factions

--- client/components/SymbolLegend.vue
+++ client/components/SymbolLegend.vue
@@ -3,5 +3,6 @@
 		<p class='[ ma0 lh-copy ]'><b>^</b> Recommended for new players</p>
 		<p class='[ ma0 lh-copy ]'><b>1</b> Prophecy of Kings</p>
 		<p class='[ ma0 lh-copy ]'><b>2</b> Codex</p>
+		<p class='[ ma0 lh-copy ]'><b>3</b> Thunder's Edge</p>
 	</div>
 </template>
